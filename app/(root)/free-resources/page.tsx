ac45e743b34fc2dc52cf613e100ea0bd5f76e049
JeronimoGinaca <jeronimo.ginaca@globant.com>
2024-01-21 19:57:34 +0100

    hola

--- app/(root)/free-resources/page.tsx
+++ app/(root)/free-resources/page.tsx
@@ -1,45 +1,9 @@
-import Filters from "@/components/component/Filters";
-import Resource from "@/components/component/Resource";
-import { SearchForm } from "@/components/component/SearchForm";
-import { getResources } from "@/sanity/actions";
+import React from 'react'
 
-export default async function Home() {
-  const resources = await getResources({
-    query: "",
-    category: "",
-    page: "1"
-  })
-
-  console.log(resources);
+const page = () => {
   return (
-    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
-      <section className="nav-padding w-full">
-        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
-          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
-            CodeHacker Academy
-          </h1>
-        </div>
-
-        <SearchForm />
-
-      </section>
-
-      <Filters/>
-
-      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          {resources?.length > 0 ? (
-            resources.map((resource: any) => (
-              <Resource />
-            ))
-          ) : (
-            <p className="body-regultar text-white-400">
-              No resources found
-            </p>
-          )}
-        </div>
-</section>
-    </main>
-
+    <div>page</div>
   )
 }
+
+export default page
