883b73028f09b86c15a6572238e73c7b19a20f0a
JeronimoGinaca <jeronimo.ginaca@globant.com>
2024-01-21 18:44:37 +0100

    test

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from '@/components/component/Footer'
+import Navbar from '@/components/component/Navbar'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode}) => {
   return (
     <>
-        Navbar 
+        <Navbar />
         {children}
-        Footer
+       <Footer />
     </>
   )
 }
